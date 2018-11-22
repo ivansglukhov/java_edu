32e276261a886e9861d2e6cb6ab761209b56f18a
ivan <43033578+ivansglukhov@users.noreply.github.com>
2018-11-23 00:52:49 +0300

    Изменён метод подсчета координат

--- sandbox/src/main/java/ru/ivan/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/ivan/pft/sandbox/Point.java
@@ -1,10 +1,22 @@
 package ru.ivan.pft.sandbox;
 
 public class Point {
-    public double x;
-    public double y;
-    public Point(double xPos,double yPos){
-        this.x=xPos;
-        this.y=yPos;
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
+    public Point(double xPos1,double yPos1,double xPos2,double yPos2){
+        this.x1=xPos1;
+        this.y1=yPos1;
+        this.x2=xPos2;
+        this.y2=yPos2;
+    }
+    public double distance(){
+        System.out.println("Вычисляем расстояние между точками");
+        double a=this.x2-this.x1;
+        double b=this.y2-this.y1;
+        a=a*a;
+        b=b*b;
+        return Math.sqrt(a+b);
     }
 }
--- sandbox/src/main/java/ru/ivan/pft/sandbox/qq.java
+++ sandbox/src/main/java/ru/ivan/pft/sandbox/qq.java
@@ -6,21 +6,19 @@ public class qq {
 		double py1 = 5.60;
 		double px2 = 2.58;
 		double py2 = 83.35;
-		Point p1 = new Point(px1, py1);
-		Point p2 = new Point(px2, py2);
-		System.out.println("Даны точки: P1(" + p1.x + "," + p1.y + ") и Р2(" + p2.x + "," + p2.y + ")");
-		System.out.println("Расстояние равно "+distance(p1,p2));
+		Point p = new Point(px1, py1,px2,py2);
+		System.out.println("Даны точки: P1(" + p.x1 + "," + p.y1 + ") и Р2(" + p.x2 + "," + p.y2 + ")");
+		System.out.println("Расстояние равно "+p.distance());
 
 	}
-	public static double distance(Point p1, Point p2) {
-		System.out.println("Вычисляем расстояние между точками");
-		double a=p2.x-p1.x;
-		double b=p2.y-p1.y;
-		a=a*a;
-		b=b*b;
-		return Math.sqrt(a+b);
-
-	}
+//	public static double distance(Point p1, Point p2) {
+//		System.out.println("Вычисляем расстояние между точками");
+//		double a=p2.x-p1.x;
+//		double b=p2.y-p1.y;
+//		a=a*a;
+//		b=b*b;
+//		return Math.sqrt(a+b);
+//	}
 
 
 }
